524bdfeeca2b2d85a0e5d3ae1a0d7f80e5acda30
Shaked Lubin <shaked.lubin@gmail.com>
2021-06-01 19:53:54 +0300

    Some graphs

--- main.py
+++ main.py
@@ -5,19 +5,35 @@ from scipy import signal
 def step(n, n0=0):
     return 1 * (n >= n0)
 
-def q3_aleph():
+def delta(n, n0=0):
+    return 1 * (np.abs(n-n0)<=0.01)
+    
+    
+def q1_D():
+    n = np.arange(-10,10)
+    h =  pow(0.5,n)*step(n) + pow(0.75,n)*step(n,2)
+    plt.xlabel('n')
+    plt.ylabel('h [n]')
+    plt.title(r'Plot of signal $h[n] = (0.5^n)u[n]+(0.75^n)u[n-2]$')
+    plt.stem(n, h)
+    plt.show()
+    
+def q3_A():
     x = np.linspace(-np.pi,np.pi,1000)
     y = 6*(step(x,-np.pi/6) - step(x,np.pi/6))
-    plt.plot(x, y).show()
+    plt.xlabel('$\Omega$')
+    plt.ylabel('$|X_1(j\Omega$)|')
+    plt.title(r'Plot of $X_1(j\Omega$)')
+    plt.plot(x, y)
+    plt.show()
+    
+    x = np.linspace(-np.pi,np.pi,1000)
+    y = np.pi*(delta(x,-np.pi/12)+delta(x,np.pi/12)+delta(x,-np.pi/6)+delta(x,np.pi/6))
+    plt.xlabel('$\Omega$')
+    plt.ylabel('$|X_2(j\Omega$)|')
+    plt.title(r'Plot of $X_2(j\Omega$)')
+    plt.plot(x, y)
+    plt.show()
 
-
-# n = np.arange(-10,10)
-# h =  pow(0.5,n)*step(n) + pow(0.75,n)*step(n,2)
-# plt.xlabel('n')
-# plt.ylabel('h [n]')
-# plt.title(r'Plot of signal $h[n] = (0.5^n)u[n]+(0.75^n)u[n-2]$')
-# plt.stem(n, h)
-# plt.show()
-q3_aleph()
-a = signal.TransferFunction([1], [1, 1], dt=1)
-print(a)
+q1_D()
+q3_A()
