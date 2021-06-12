2416598ee7e4f0f5363a598b49a53aa3f699df68
Shaked Lubin <shaked.lubin@gmail.com>
2021-06-12 11:26:26 +0300

    Code for Be

--- SignalsPartBe.m
+++ SignalsPartBe.m
@@ -14,30 +14,29 @@ t_sampled = T_s*n;
 x = 2*cos((3*pi/10)*n).*cos((pi/10)*n);
 
 %y[n]=convolution(x[n],hi[n])
-y2 = conv(x,h2);
-y3 = conv(x,h3);
+y2 = conv(x,h2,'same');
+y3 = conv(x,h3,'same');
 y4 = conv(x,h4,'same');
 y6 = conv(x,h6,'same');
 
 %Plotting
-t_max = 10;
-Taxis = linspace(-t_max,t_max);
-% nexttile
-% plot(n,x,'b*-');
-% hold on;
-% plot(n,y2, 'r*-');
-% title('x[n] and y_2[n]');
-% xlabel('n[sec]');
-
 nexttile
 plot(t_sampled, x);
 hold on;
 plot(t_sampled, y4);
+legend('x[n]','y_4[n]');
 hold off;
 title('x[n] and y_4[n]');
 xlabel('n[sec]');
-% xlim(-3:3)
 
+nexttile
+plot(t_sampled, x);
+hold on;
+plot(t_sampled, y6);
+legend('x[n]','y_6[n]');
+hold off;
+title('x[n] and y_6[n]');
+xlabel('n[sec]');
 
 
 
